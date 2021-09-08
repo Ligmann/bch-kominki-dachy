96d031c152cdecc59b07a7aa6c44ce9a5cb3f344
Damian Ligmann <ligmann.damian@gmail.com>
2021-09-08 16:50:33 +0200

    add page-blank

---
+++ page-blank/javascript/main.js
@@ -0,0 +1,2 @@
+"use strict";window.fbAsyncInit=function(){FB.init({xfbml:!0,version:"v9.0"})},function(a,b,c){var d,e=a.getElementsByTagName(b)[0];a.getElementById(c)||(d=a.createElement(b),d.id=c,d.src="https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js",e.parentNode.insertBefore(d,e))}(document,"script","facebook-jssdk");
+//# sourceMappingURL=main.js.map
